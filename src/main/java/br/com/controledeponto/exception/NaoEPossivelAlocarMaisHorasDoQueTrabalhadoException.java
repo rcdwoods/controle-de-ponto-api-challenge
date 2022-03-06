5dc990af8b0b26a2b334c7932b60a7e51971de3d
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 20:22:57 -0300

    :hammer: Criar novas Exceções

---
+++ src/main/java/br/com/controledeponto/exception/NaoEPossivelAlocarMaisHorasDoQueTrabalhadoException.java
@@ -0,0 +1,7 @@
+package br.com.controledeponto.exception;
+
+public class NaoEPossivelAlocarMaisHorasDoQueTrabalhadoException extends Exception {
+	public NaoEPossivelAlocarMaisHorasDoQueTrabalhadoException(String mensagem) {
+		super(mensagem);
+	}
+}
---
+++ src/main/java/br/com/controledeponto/exception/NaoPossuiTempoDisponivelParaAlocacaoException.java
@@ -0,0 +1,7 @@
+package br.com.controledeponto.exception;
+
+public class NaoPossuiTempoDisponivelParaAlocacaoException extends Exception {
+	public NaoPossuiTempoDisponivelParaAlocacaoException(String mensagem) {
+		super(mensagem);
+	}
+}
