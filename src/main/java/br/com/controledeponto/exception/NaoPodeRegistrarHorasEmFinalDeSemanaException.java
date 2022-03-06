66e1f1975e5a708f787a3b28cd0a257036a86ad9
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-05 22:38:53 -0300

    :hammer: Criar classe de Exceção NaoPodeRegistrarHorasEmFinalDeSemanaException

---
+++ src/main/java/br/com/controledeponto/exception/NaoPodeRegistrarHorasEmFinalDeSemanaException.java
@@ -0,0 +1,7 @@
+package br.com.controledeponto.exception;
+
+public class NaoPodeRegistrarHorasEmFinalDeSemanaException extends Exception {
+	public NaoPodeRegistrarHorasEmFinalDeSemanaException(String mensagem) {
+		super(mensagem);
+	}
+}
