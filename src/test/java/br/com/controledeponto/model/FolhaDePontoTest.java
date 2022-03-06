c6c6840cef5ba6128ce998d68d7ad19978efa5e9
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 12:46:36 -0300

    :white_check_mark: Adicionar teste para validar cálculo das horas trabalhadas

--- src/test/java/br/com/controledeponto/model/FolhaDePontoTest.java
+++ src/test/java/br/com/controledeponto/model/FolhaDePontoTest.java
@@ -1,6 +1,18 @@
 package br.com.controledeponto.model;
 
 class FolhaDePontoTest {
+
+	@Test
+	void deveCalcularHorasTotaisTrabalhadasNoMes() throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		List<RegistroDeTrabalho> registrosDeTrabalho = List.of(
+			criarRegistroDeTrabalhoCompleto("2021-01-04"),
+			criarRegistroDeTrabalhoCompleto("2021-01-05"),
+			criarRegistroDeTrabalhoCompleto("2021-01-06")
+		);
+		FolhaDePonto folhaDePonto = new FolhaDePonto(YearMonth.parse("2021-01"), registrosDeTrabalho, List.of());
+		Assertions.assertThat(folhaDePonto.getHorasTrabalhadas()).isEqualTo(Duration.parse("PT24H0M0S"));
+	}
+
 	private RegistroDeTrabalho criarRegistroDeTrabalhoCompleto(String data) throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
 		RegistroDeTrabalho registroDeTrabalho = new RegistroDeTrabalho(LocalDate.parse(data));
 		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T09:00:00")));
