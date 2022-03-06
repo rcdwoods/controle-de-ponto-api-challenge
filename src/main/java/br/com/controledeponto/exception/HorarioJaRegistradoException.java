ffbe726b2da73d1540cab3325ef6aee485c95023
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-05 22:28:21 -0300

    :hammer: Criar classe de exceção HorarioJaRegistradoException

---
+++ src/main/java/br/com/controledeponto/exception/HorarioJaRegistradoException.java
@@ -0,0 +1,7 @@
+package br.com.controledeponto.exception;
+
+public class HorarioJaRegistradoException extends Exception{
+	public HorarioJaRegistradoException(String mensagem) {
+		super(mensagem);
+	}
+}
