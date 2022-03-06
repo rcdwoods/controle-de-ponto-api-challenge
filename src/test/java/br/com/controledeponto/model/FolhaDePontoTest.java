8d6d7cd9328762937044e2fbf10a1a84d0678225
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 12:45:52 -0300

    :white_check_mark: Adicionar método privado para criar registros de trabalho completos

--- src/test/java/br/com/controledeponto/model/FolhaDePontoTest.java
+++ src/test/java/br/com/controledeponto/model/FolhaDePontoTest.java
@@ -1,4 +1,12 @@
 package br.com.controledeponto.model;
 
 class FolhaDePontoTest {
+	private RegistroDeTrabalho criarRegistroDeTrabalhoCompleto(String data) throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		RegistroDeTrabalho registroDeTrabalho = new RegistroDeTrabalho(LocalDate.parse(data));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T09:00:00")));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T12:00:00")));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T13:00:00")));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T18:00:00")));
+		return registroDeTrabalho;
+	}
 }
