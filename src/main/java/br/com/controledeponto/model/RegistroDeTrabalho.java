e595b4e7f11d499bfb9808bc084d7998d55be574
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-05 22:20:28 -0300

    :hammer: Adicionar método para registrar momentos no RegistroDeTrabalho

--- src/main/java/br/com/controledeponto/model/RegistroDeTrabalho.java
+++ src/main/java/br/com/controledeponto/model/RegistroDeTrabalho.java
@@ -1,8 +1,19 @@
 package br.com.controledeponto.model;
 
+import br.com.controledeponto.exception.DeveHaverNoMinimoUmaHoraDeAlmocoException;
+import br.com.controledeponto.exception.HorarioInferiorAoUltimoRegistradoException;
+import br.com.controledeponto.exception.HorarioJaRegistradoException;
+import br.com.controledeponto.exception.NaoPodeHaverMaisDeQuatroRegistrosException;
+import br.com.controledeponto.exception.NaoPodeRegistrarHorasEmFinalDeSemanaException;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RegistroDeTrabalho {
 	private LocalDate dia;
@@ -10,5 +21,14 @@ public class RegistroDeTrabalho {
 
 	RegistroDeTrabalho(LocalDate dia) {
 		this.dia = dia;
+	}
+
+	List<Momento> getMomentosRegistrados() {
+		return this.momentosRegistrados;
+	}
+
+	List<Momento> registrarMomento(Momento momento) throws HorarioInferiorAoUltimoRegistradoException, HorarioJaRegistradoException, NaoPodeHaverMaisDeQuatroRegistrosException, DeveHaverNoMinimoUmaHoraDeAlmocoException, NaoPodeRegistrarHorasEmFinalDeSemanaException {
+		this.momentosRegistrados.add(momento);
+		return this.momentosRegistrados;
 	}
 }
