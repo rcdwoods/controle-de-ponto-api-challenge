aa920579e089e1079e90afd53b21455cf6b1accb
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 20:17:51 -0300

    :white_check_mark: Criar testes para a classe MomentoService

---
+++ src/test/java/br/com/controledeponto/service/MomentoServiceTest.java
@@ -0,0 +1,61 @@
+package br.com.controledeponto.service;
+
+import br.com.controledeponto.exception.DeveHaverNoMinimoUmaHoraDeAlmocoException;
+import br.com.controledeponto.exception.HorarioInferiorAoUltimoRegistradoException;
+import br.com.controledeponto.exception.HorarioJaRegistradoException;
+import br.com.controledeponto.exception.NaoPodeHaverMaisDeQuatroRegistrosException;
+import br.com.controledeponto.exception.NaoPodeRegistrarHorasEmFinalDeSemanaException;
+import br.com.controledeponto.model.Momento;
+import br.com.controledeponto.model.RegistroDeTrabalho;
+import br.com.controledeponto.repository.MomentoRepository;
+import br.com.controledeponto.service.impl.MomentoServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ContextConfiguration(
+	classes = MomentoServiceImpl.class
+)
+@ExtendWith(SpringExtension.class)
+class MomentoServiceTest {
+
+	@Autowired
+	private MomentoService momentoService;
+
+	@MockBean
+	private MomentoRepository momentoRepository;
+
+	@MockBean
+	private RegistroDeTrabalhoService registroDeTrabalhoService;
+
+	@BeforeEach
+	void setup() {
+		Mockito.when(momentoRepository.save(Mockito.any())).thenAnswer(i -> i.getArgument(0));
+	}
+
+	@Test
+	void deveSalvarMomentoEAdicionaloAoRegistroDeTrabalhoNoDia() throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		Momento momento = new Momento(LocalDateTime.parse("2021-01-01T08:00:00"));
+
+		Mockito.when(registroDeTrabalhoService.adicionarMomentoAoSeuRegistroDeTrabalho(momento))
+				.thenReturn(new RegistroDeTrabalho(LocalDate.parse("2021-01-01")));
+
+		Momento momentoSalvo = momentoService.salvarMomento(momento);
+
+		Mockito.verify(registroDeTrabalhoService, Mockito.times(1)).adicionarMomentoAoSeuRegistroDeTrabalho(momento);
+		Assertions.assertThat(momentoSalvo).isEqualTo(momento);
+		Assertions.assertThat(momentoSalvo.getRegistroDeTrabalho()).isNotNull();
+	}
+}
