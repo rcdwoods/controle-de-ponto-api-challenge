65a07771508dc1f4031c36b84433ee0e9820b5b6
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 12:40:50 -0300

    :hammer: Adicionar construtor à classe Alocacao

--- src/main/java/br/com/controledeponto/model/Alocacao.java
+++ src/main/java/br/com/controledeponto/model/Alocacao.java
@@ -1,9 +1,16 @@
 package br.com.controledeponto.model;
 
+import java.time.Duration;
 import java.time.LocalDate;
 
 public class Alocacao {
 	private LocalDate dia;
-	private String tempo;
+	private Duration tempo;
 	private String nomeProjeto;
+
+	Alocacao(LocalDate dia, Duration tempo, String nomeProjeto) {
+		this.dia = dia;
+		this.tempo = tempo;
+		this.nomeProjeto = nomeProjeto;
+	}
 }
