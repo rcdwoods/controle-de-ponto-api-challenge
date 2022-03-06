47b3b0d52c8fbb5dbda5c674279f4d3ca7e6abec
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 12:51:35 -0300

    :white_check_mark: Adicionar testes para validar cálculo de horas devidas

--- src/test/java/br/com/controledeponto/model/FolhaDePontoTest.java
+++ src/test/java/br/com/controledeponto/model/FolhaDePontoTest.java
@@ -1,5 +1,19 @@
 package br.com.controledeponto.model;
 
+import br.com.controledeponto.exception.DeveHaverNoMinimoUmaHoraDeAlmocoException;
+import br.com.controledeponto.exception.HorarioInferiorAoUltimoRegistradoException;
+import br.com.controledeponto.exception.HorarioJaRegistradoException;
+import br.com.controledeponto.exception.NaoPodeHaverMaisDeQuatroRegistrosException;
+import br.com.controledeponto.exception.NaoPodeRegistrarHorasEmFinalDeSemanaException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.util.List;
+
 class FolhaDePontoTest {
 
 	@Test
@@ -45,6 +59,40 @@ class FolhaDePontoTest {
 		FolhaDePonto folhaDePonto = new FolhaDePonto(YearMonth.parse("2021-01"), registrosDeTrabalho, List.of());
 		Assertions.assertThat(folhaDePonto.getHorasExcedentes()).isEqualTo(Duration.parse("PT0S"));
 	}
+
+	@Test
+	void deveGerarHorasDevidasQuandoFuncionarioTrabalharAMenos() throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		List<RegistroDeTrabalho> registrosDeTrabalho = List.of(
+			criarRegistroDeTrabalhoIncompleto("2021-01-04"),
+			criarRegistroDeTrabalhoIncompleto("2021-01-05"),
+			criarRegistroDeTrabalhoIncompleto("2021-01-06")
+		);
+		FolhaDePonto folhaDePonto = new FolhaDePonto(YearMonth.parse("2021-01"), registrosDeTrabalho, List.of());
+		Assertions.assertThat(folhaDePonto.getHorasDevidas()).isEqualTo(Duration.parse("PT4H31M30S"));
+	}
+
+	@Test
+	void naoDeveGerarHorasDevidasQuandoFuncionarioTrabalharHorarioCompleto() throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		List<RegistroDeTrabalho> registrosDeTrabalho = List.of(
+			criarRegistroDeTrabalhoCompleto("2021-01-04"),
+			criarRegistroDeTrabalhoCompleto("2021-01-05"),
+			criarRegistroDeTrabalhoCompleto("2021-01-06")
+		);
+		FolhaDePonto folhaDePonto = new FolhaDePonto(YearMonth.parse("2021-01"), registrosDeTrabalho, List.of());
+		Assertions.assertThat(folhaDePonto.getHorasDevidas()).isEqualTo(Duration.parse("PT0S"));
+	}
+
+	@Test
+	void naoDeveGerarHorasDevidasQuandoFuncionarioTrabalharAMais() throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		List<RegistroDeTrabalho> registrosDeTrabalho = List.of(
+			criarRegistroDeTrabalhoExcedente("2021-01-04"),
+			criarRegistroDeTrabalhoExcedente("2021-01-05"),
+			criarRegistroDeTrabalhoExcedente("2021-01-06")
+		);
+		FolhaDePonto folhaDePonto = new FolhaDePonto(YearMonth.parse("2021-01"), registrosDeTrabalho, List.of());
+		Assertions.assertThat(folhaDePonto.getHorasDevidas()).isEqualTo(Duration.parse("PT0S"));
+	}
+
 	private RegistroDeTrabalho criarRegistroDeTrabalhoCompleto(String data) throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
 		RegistroDeTrabalho registroDeTrabalho = new RegistroDeTrabalho(LocalDate.parse(data));
 		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T09:00:00")));
@@ -63,4 +111,12 @@ class FolhaDePontoTest {
 		return registroDeTrabalho;
 	}
 
+	private RegistroDeTrabalho criarRegistroDeTrabalhoIncompleto(String data) throws NaoPodeHaverMaisDeQuatroRegistrosException, NaoPodeRegistrarHorasEmFinalDeSemanaException, HorarioInferiorAoUltimoRegistradoException, DeveHaverNoMinimoUmaHoraDeAlmocoException, HorarioJaRegistradoException {
+		RegistroDeTrabalho registroDeTrabalho = new RegistroDeTrabalho(LocalDate.parse(data));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T09:00:00")));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T12:00:00")));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T13:00:00")));
+		registroDeTrabalho.registrarMomento(new Momento(LocalDateTime.parse(data + "T16:29:30")));
+		return registroDeTrabalho;
+	}
 }
