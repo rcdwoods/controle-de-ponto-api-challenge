f060e85eb3a9c23c6533b997680fdc4071dc1c15
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 12:43:39 -0300

    :hammer: Substituir tipo das variáveis de horas para Duration

--- src/main/java/br/com/controledeponto/model/FolhaDePonto.java
+++ src/main/java/br/com/controledeponto/model/FolhaDePonto.java
@@ -1,13 +1,14 @@
 package br.com.controledeponto.model;
 
+import java.time.Duration;
 import java.time.YearMonth;
 import java.util.List;
 
 public class FolhaDePonto {
 	private YearMonth mes;
-	private String horasTrabalhadas;
-	private String horasExcedentes;
-	private String horasDevidas;
+	private Duration horasTrabalhadas;
+	private Duration horasExcedentes;
+	private Duration horasDevidas;
 	private List<RegistroDeTrabalho> registrosDeTrabalho;
 	private List<Alocacao> alocacoes;
 
