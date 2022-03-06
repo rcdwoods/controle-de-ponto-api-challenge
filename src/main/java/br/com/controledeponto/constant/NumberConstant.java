932c2068a9de4f51ab9f437fdf8ebce17fdd0f20
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-06 12:41:11 -0300

    :hammer: Criar classe de constantes para representar numberos

---
+++ src/main/java/br/com/controledeponto/constant/NumberConstant.java
@@ -0,0 +1,8 @@
+package br.com.controledeponto.constant;
+
+public final class NumberConstant {
+	public static final int ZERO = 0;
+	public static final int ONE = 1;
+	public static final int TWO = 2;
+	public static final int THREE= 3;
+}
