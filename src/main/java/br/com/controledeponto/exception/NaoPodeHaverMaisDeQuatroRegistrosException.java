953f10b46c4782a123d40f531c1c81d2c6e05e40
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-05 22:32:37 -0300

    :hammer: Criar classe de Exception NaoPodeHaverMaisDeQuatroRegistrosException

---
+++ src/main/java/br/com/controledeponto/exception/NaoPodeHaverMaisDeQuatroRegistrosException.java
@@ -0,0 +1,7 @@
+package br.com.controledeponto.exception;
+
+public class NaoPodeHaverMaisDeQuatroRegistrosException extends Exception {
+	public NaoPodeHaverMaisDeQuatroRegistrosException(String mensagem) {
+		super(mensagem);
+	}
+}
