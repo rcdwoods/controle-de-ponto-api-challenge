c66a68e1cc3aaca95ae4d2bfc1422d532c59c6e5
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-05 22:33:47 -0300

    :hammer: Criar classe de Exception DeveHaverNoMinimoUmaHoraDeAlmocoException

---
+++ src/main/java/br/com/controledeponto/exception/DeveHaverNoMinimoUmaHoraDeAlmocoException.java
@@ -0,0 +1,7 @@
+package br.com.controledeponto.exception;
+
+public class DeveHaverNoMinimoUmaHoraDeAlmocoException extends Exception {
+	public DeveHaverNoMinimoUmaHoraDeAlmocoException(String mensagem) {
+		super(mensagem);
+	}
+}
