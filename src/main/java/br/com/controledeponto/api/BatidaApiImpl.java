6878800f63a38a92101dad330a5a246277356a11
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-12 17:01:28 -0300

    :hammer: Adicionar endpoint para batida de ponto

--- src/main/java/br/com/controledeponto/api/BatidaApiImpl.java
+++ src/main/java/br/com/controledeponto/api/BatidaApiImpl.java
@@ -1,17 +1,34 @@
 package br.com.controledeponto.api;
 
+import br.com.controledeponto.converter.MomentoConverter;
+import br.com.controledeponto.service.BatidaService;
 import io.swagger.api.BatidasApi;
 import io.swagger.model.Momento;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/batidas")
-public class BatidasApiImpl implements BatidasApi {
+import javax.validation.Valid;
 
-	@Override
-	public ResponseEntity<Momento> baterPonto(Momento momento) {
-		return BatidasApi.super.baterPonto(momento);
+@RestController
+@RequestMapping("/v1/batidas")
+public class BatidaApiImpl implements BatidasApi {
+
+	private BatidaService batidaService;
+
+	private MomentoConverter momentoConverter;
+
+	public BatidaApiImpl(BatidaService batidaService, MomentoConverter momentoConverter) {
+		this.batidaService = batidaService;
+		this.momentoConverter = momentoConverter;
+	}
+
+	@PostMapping
+	public ResponseEntity<Momento> baterPonto(@Valid Momento momento) {
+		br.com.controledeponto.model.Momento momentoConvertido = momentoConverter.converter(momento);
+		Momento momentoSalvo = momentoConverter.converter(batidaService.salvarMomento(momentoConvertido));
+		return ResponseEntity.status(HttpStatus.CREATED).body(momentoSalvo);
 	}
 }
