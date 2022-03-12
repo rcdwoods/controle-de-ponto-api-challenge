199ceab08a696f8963adbb1a7c7d5918b846d942
Richard Pereira do Nascimento <rcdwoods@gmail.com>
2022-03-12 18:20:13 -0300

    :hammer: Criar classe para conversão do objeto Alocacao

--- src/main/java/br/com/controledeponto/converter/AlocacaoConverter.java
+++ src/main/java/br/com/controledeponto/converter/AlocacaoConverter.java
@@ -1,2 +1,10 @@
-package br.com.controledeponto.converter;public interface AlocacaoConverter {
+package br.com.controledeponto.converter;
+
+import br.com.controledeponto.model.Alocacao;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AlocacaoConverter {
+	Alocacao converter(io.swagger.model.Alocacao alocacao);
+	io.swagger.model.Alocacao converter(Alocacao alocacao);
 }
